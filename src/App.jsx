e1a15e3b2079fe43f4f9b88643c348960aa414a1
suryakapoor124 <suryakapoor124@gamil.com>
2026-04-22 10:32:46 +0530

    chore: initial vite react project  structure

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      {/* This is where your Homepage components will go */}
+      <h1>Samajika Social Media</h1>
+    </div>
+  )
+}
+
+export default App
